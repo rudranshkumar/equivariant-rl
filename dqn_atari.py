5ef0d08ddf3220ab6a1de35b8d4c376f12717506
DominicKlukas <klukasdominic@gmail.com>
2025-11-12 11:40:39 -0800

    Implemented Snake DQN

---
+++ dqn_atari.py
@@ -0,0 +1,317 @@
+# docs and experiment results can be found at https://docs.cleanrl.dev/rl-algorithms/ppo/#ppo_ataripy
+import os
+os.environ["SDL_VIDEODRIVER"]="dummy"
+import random
+import time
+from dataclasses import dataclass
+
+import multiprocessing as mp
+mp.set_start_method("spawn", force=True)  # key fix
+
+import gymnasium as gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import tyro
+from torch.distributions.categorical import Categorical
+from torch.utils.tensorboard import SummaryWriter
+
+from cleanrl_utils.buffers import ReplayBuffer
+
+from ple.games.snake import Snake
+from ple import PLE
+from preprocessing.snake_preprocessing import frame_history
+
+
+class SnakeEnv(gym.Env):
+    def __init__(self, height=32, width=32, fps=15, frame_history_size=4):
+    # create the game environment and initialize the attribute values
+        super().__init__()
+        self.game = Snake(height=height,width=width,init_length=4)
+        reward_dict = {"positive": 1.0, "negative": -1.0, "tick": 0.0, "loss": -1.0, "win": 1.0}
+        self.environment = PLE(self.game, fps=fps, reward_values=reward_dict, num_steps=2, display_screen=False)
+        self.init_env() # initialize the game
+        self.allowed_actions = self.environment.getActionSet() # the list of allowed actions to be taken by an agent
+        self.num_actions = len(self.allowed_actions) - 1  # number of actions that are allowed in this env
+        self.frame_hist = frame_history(height=height,width=width,frame_history_size=frame_history_size,num_channels=3);
+        self.input_shape = self.frame_hist.get_history().shape  # shape of the game input screen
+
+        # Variables for accumulation
+        self.episode_length = 0
+        self.episode_reward = 0
+
+        # Metadata
+        self.action_space = gym.spaces.Discrete(self.num_actions)
+        self.observation_space = gym.spaces.Box(
+                    low=0, high=255, shape=(frame_history_size*3, height, width), dtype=np.uint8)
+
+    def init_env(self):
+        # initialize the variables and screen of the game
+        self.environment.init()
+
+
+    def get_current_state(self):
+    # get the current state in the game. Returns the current screen of the game with snake and food positions with
+    # a sequence of past .
+        cur_frame = np.transpose(self.environment.getScreenRGB(),(2,0,1))
+        #cur_frame = np.transpose(np.expand_dims(self.environment.getScreenGrayscale(),axis=0), (2, 0, 1))
+        self.frame_hist.push(cur_frame)
+        return self.frame_hist.get_history()
+
+
+    def check_game_over(self):
+        # check if the game has terminated
+        return self.environment.game_over()
+
+    def reset(self, seed=None, options=None):
+        # resets the game to initial values and refreshes the screen with a new small snake and random food position.
+        self.environment.reset_game()
+        _ = self.environment.act(None)
+        self.frame_hist.reset(np.transpose(self.environment.getScreenRGB(),(2,0,1)))
+        self.episode_reward = 0
+        self.episode_length = 0
+        return self.frame_hist.get_history(), {}
+
+    def step(self, action):
+        # lets the snake take the chosen action of moving in some direction
+        reward = self.environment.act(self.allowed_actions[action])
+        self.episode_reward += reward
+        self.episode_length += 1
+        next_state = self.get_current_state()
+        done = self.check_game_over()
+        info = {}
+        if done:
+            info['episode'] = {"r" : self.episode_reward, "l" : self.episode_length}
+        return next_state, reward, done, False, info
+
+@dataclass
+class Args:
+    exp_name: str = os.path.basename(__file__)[: -len(".py")]
+    """the name of this experiment"""
+    seed: int = 1
+    """seed of the experiment"""
+    torch_deterministic: bool = True
+    """if toggled, `torch.backends.cudnn.deterministic=False`"""
+    cuda: bool = True
+    """if toggled, cuda will be enabled by default"""
+    track: bool = False
+    """if toggled, this experiment will be tracked with Weights and Biases"""
+    wandb_project_name: str = "cleanRL"
+    """the wandb's project name"""
+    wandb_entity: str = None
+    """the entity (team) of wandb's project"""
+    capture_video: bool = False
+    """whether to capture videos of the agent performances (check out `videos` folder)"""
+    save_model: bool = False
+    """whether to save model into the `runs/{run_name}` folder"""
+    upload_model: bool = False
+    """whether to upload the saved model to huggingface"""
+    hf_entity: str = ""
+    """the user or org name of the model repository from the Hugging Face Hub"""
+
+    # Algorithm specific arguments
+    env_id: str = "BreakoutNoFrameskip-v4"
+    """the id of the environment"""
+    total_timesteps: int = 10000000
+    """total timesteps of the experiments"""
+    learning_rate: float = 1e-4
+    """the learning rate of the optimizer"""
+    num_envs: int = 1
+    """the number of parallel game environments"""
+    buffer_size: int = 1000000
+    """the replay memory buffer size"""
+    gamma: float = 0.99
+    """the discount factor gamma"""
+    tau: float = 1.0
+    """the target network update rate"""
+    target_network_frequency: int = 1000
+    """the timesteps it takes to update the target network"""
+    batch_size: int = 32
+    """the batch size of sample from the reply memory"""
+    start_e: float = 1
+    """the starting epsilon for exploration"""
+    end_e: float = 0.01
+    """the ending epsilon for exploration"""
+    exploration_fraction: float = 0.10
+    """the fraction of `total-timesteps` it takes from start-e to go end-e"""
+    learning_starts: int = 80000
+    """timestep to start learning"""
+    train_frequency: int = 4
+    """the frequency of training"""
+def make_env():
+    def thunk():
+        env = SnakeEnv()
+        return env
+    return thunk
+
+
+# ALGO LOGIC: initialize agent here:
+class QNetwork(nn.Module):
+    def __init__(self, env):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Conv2d(12, 32, 7, stride=2, padding=2),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 5, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(3136, 256),
+            nn.ReLU(),
+            nn.Linear(256, env.single_action_space.n),
+        )
+
+    def forward(self, x):
+        return self.network(x / 255.0)
+
+
+def linear_schedule(start_e: float, end_e: float, duration: int, t: int):
+    slope = (end_e - start_e) / duration
+    return max(slope * t + start_e, end_e)
+
+
+if __name__ == "__main__":
+    args = tyro.cli(Args)
+    assert args.num_envs == 1, "vectorized envs are not supported at the moment"
+    run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+    if args.track:
+        import wandb
+
+        wandb.init(
+            project=args.wandb_project_name,
+            entity=args.wandb_entity,
+            sync_tensorboard=True,
+            config=vars(args),
+            name=run_name,
+            monitor_gym=True,
+            save_code=True,
+        )
+    writer = SummaryWriter(f"runs/{run_name}")
+    writer.add_text(
+        "hyperparameters",
+        "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+    )
+
+    # TRY NOT TO MODIFY: seeding
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = args.torch_deterministic
+
+    device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+
+    # env setup
+    envs = gym.vector.SyncVectorEnv(
+        [make_env() for i in range(args.num_envs)]
+    )
+    #assert isinstance(envs.single_action_space, gym.spaces.Discrete), "only discrete action space is supported"
+
+    q_network = QNetwork(envs).to(device)
+    optimizer = optim.Adam(q_network.parameters(), lr=args.learning_rate)
+    target_network = QNetwork(envs).to(device)
+    target_network.load_state_dict(q_network.state_dict())
+
+    rb = ReplayBuffer(
+        args.buffer_size,
+        envs.single_observation_space,
+        envs.single_action_space,
+        device,
+        optimize_memory_usage=True,
+        handle_timeout_termination=False,
+    )
+    start_time = time.time()
+
+    # TRY NOT TO MODIFY: start the game
+    obs, _ = envs.reset(seed=args.seed)
+    for global_step in range(args.total_timesteps):
+        # ALGO LOGIC: put action logic here
+        epsilon = linear_schedule(args.start_e, args.end_e, args.exploration_fraction * args.total_timesteps, global_step)
+        if random.random() < epsilon:
+            actions = np.array([envs.single_action_space.sample() for _ in range(envs.num_envs)])
+        else:
+            q_values = q_network(torch.Tensor(obs).to(device))
+            actions = torch.argmax(q_values, dim=1).cpu().numpy()
+
+        # TRY NOT TO MODIFY: execute the game and log data.
+        next_obs, rewards, terminations, truncations, infos = envs.step(actions)
+
+        # TRY NOT TO MODIFY: record rewards for plotting purposes
+        if "final_info" in infos:
+            for info in infos["final_info"]:
+                if info and "episode" in info:
+                    print(f"global_step={global_step}, episodic_return={info['episode']['r']}")
+                    writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
+                    writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)
+
+        # TRY NOT TO MODIFY: save data to reply buffer; handle `final_observation`
+        real_next_obs = next_obs.copy()
+        for idx, trunc in enumerate(truncations):
+            if trunc:
+                print(trunc, True)
+                real_next_obs[idx] = infos["final_observation"][idx]
+        rb.add(obs, real_next_obs, actions, rewards, terminations, infos)
+
+        # TRY NOT TO MODIFY: CRUCIAL step easy to overlook
+        obs = next_obs
+
+        # ALGO LOGIC: training.
+        if global_step > args.learning_starts:
+            if global_step % args.train_frequency == 0:
+                data = rb.sample(args.batch_size)
+                with torch.no_grad():
+                    target_max, _ = target_network(data.next_observations).max(dim=1)
+                    td_target = data.rewards.flatten() + args.gamma * target_max * (1 - data.dones.flatten())
+                old_val = q_network(data.observations).gather(1, data.actions).squeeze()
+                loss = F.mse_loss(td_target, old_val)
+
+                if global_step % 100 == 0:
+                    writer.add_scalar("losses/td_loss", loss, global_step)
+                    writer.add_scalar("losses/q_values", old_val.mean().item(), global_step)
+                    print("SPS:", int(global_step / (time.time() - start_time)))
+                    writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
+
+                # optimize the model
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            # update target network
+            if global_step % args.target_network_frequency == 0:
+                for target_network_param, q_network_param in zip(target_network.parameters(), q_network.parameters()):
+                    target_network_param.data.copy_(
+                        args.tau * q_network_param.data + (1.0 - args.tau) * target_network_param.data
+                    )
+
+            """
+    if args.save_model:
+        model_path = f"runs/{run_name}/{args.exp_name}.cleanrl_model"
+        torch.save(q_network.state_dict(), model_path)
+        print(f"model saved to {model_path}")
+        from cleanrl_utils.evals.dqn_eval import evaluate
+
+        episodic_returns = evaluate(
+            model_path,
+            make_env,
+            args.env_id,
+            eval_episodes=10,
+            run_name=f"{run_name}-eval",
+            Model=QNetwork,
+            device=device,
+            epsilon=args.end_e,
+        )
+        for idx, episodic_return in enumerate(episodic_returns):
+            writer.add_scalar("eval/episodic_return", episodic_return, idx)
+
+        if args.upload_model:
+            from cleanrl_utils.huggingface import push_to_hub
+
+            repo_name = f"{args.env_id}-{args.exp_name}-seed{args.seed}"
+            repo_id = f"{args.hf_entity}/{repo_name}" if args.hf_entity else repo_name
+            push_to_hub(args, episodic_returns, repo_id, "DQN", f"runs/{run_name}", f"videos/{run_name}-eval")
+            """
+
+    envs.close()
+    writer.close()
---
+++ preprocessing/snake_preprocessing.py
@@ -0,0 +1,29 @@
+import collections
+import numpy as np
+
+__all__ = ["frame_history"]
+
+class frame_history():
+    def __init__(self, height=32, width =32, frame_history_size=4, num_channels=3):
+        self.buffer = collections.deque([np.zeros((num_channels, height, width)) for i in range(frame_history_size)])
+        self.height = height
+        self.width = width
+        self.frame_history_size = frame_history_size
+
+    def reset(self,frame):
+        self.buffer.clear()
+        self.buffer.extend([frame for i in range(self.frame_history_size)])
+
+    def push(self, frame):
+        self.buffer.popleft()
+        self.buffer.append(frame)
+
+    def get_history(self):
+        return np.asarray(self.buffer).reshape((-1, self.height, self.width))
+
+    def clear(self):
+        self.buffer.clear()
+
+    def __len__(self):
+        return len(self.buffer)
+
